1205f1911dbd84266f4a7c273f49f4a1dbbc8416
shubhamvaishnav18 <shubhamvaishnav332@gmail.com>
2026-02-05 04:14:43 +0530

    adjust db for vercel

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 export const connectDB = async () => {
+  if (isConnected) return;
+
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
+    isConnected = true;
+    console.log("MongoDB connected");
   } catch (err) {
-    console.error(err);
+    console.error("MongoDB connection failed", err);
+    throw err;
   }
 };
