5880e184b0fb3227caad639dc6a44200b43bdde7
shubhamvaishnav18 <shubhamvaishnav332@gmail.com>
2026-02-05 18:58:58 +0530

    solve images uploade error

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,10 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 import authRoutes from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
@@ -9,6 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 
